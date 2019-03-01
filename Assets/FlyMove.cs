d091428ec769b4bf78d60e3e1d732fc009d05a84
masterrey <masterrey@gmail.com>
2019-03-01 08:44:24 -0300

    Subindo projeto da tie

---
+++ Assets/FlyMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyMove : MonoBehaviour
+{
+    Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"), 0);
+
+
+        transform.Translate(movement * Time.deltaTime * 40);
+    }
+
+    
+
+}
